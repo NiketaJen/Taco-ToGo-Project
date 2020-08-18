e5552cc08250840fb473b1ec846b2046bcd67b53
github username <github email address>
2020-08-18 17:04:00 -0500

    Components done.

---
+++ taco-togo-frontend/src/components/MenuCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MenuCard extends React.Component {
+  render() {
+    return <div className="MenuCard"></div>;
+  }
+}
+
+export default MenuCard;
---
+++ taco-togo-frontend/src/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SignUp extends React.Component {
+  render() {
+    return <div className="SignUp"></div>;
+  }
+}
+
+export default SignUp;
